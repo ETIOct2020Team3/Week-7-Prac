3f197ec4a60f2f77ccfd7218df9203f57f19c480
harith md noor <harithmdnoor@gmail.com>
2020-12-02 15:25:35 +0800

    a

--- test_add.py
+++ test_add.py
@@ -2,7 +2,7 @@ from fn import *
 import pytest
 
 
-@pytest.mark.parametrize("a,b,result",[(1,2,3), (2,4,6)])
+# @pytest.mark.parametrize("a,b,result",[(1,2,3), (2,4,6)])
 # def test_add(a,b,result):
 #     value = add(a,b)
 #     assert value == result
@@ -12,10 +12,10 @@ import pytest
 # def test_add_negative_negative():
 #     value = add(-1,-2)
 #     assert value == -5
-# def test_div():
-#     value = divide(10,2)
-#     assert value == 5
+def test_div():
+    value = divide(10,2)
+    assert value == 5
 
-def test_over_ten_variables():#add comment
-    value = add(1,2,3,4,5,6,7,8,9,10,11, 12)
-    assert value == "Error: >10 Values"
+# def test_over_ten_variables():#add comment
+#     value = add(1,2,3,4,5,6,7,8,9,10,11, 12)
+#     assert value == "Error: >10 Values"
